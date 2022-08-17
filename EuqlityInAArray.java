786236c2acc2524d15aa8871afcd39c993814b5f
Ryan Lo <lowc1012@gmail.com>
2022-08-17 21:13:22 +0800

    resolve equal & equality-in-a-array

---
+++ EuqlityInAArray.java
@@ -0,0 +1,60 @@
+// https://www.hackerrank.com/challenges/equality-in-a-array/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equalizeArray(List<Integer> arr) {
+        // 1 <= arr.size() <= 100
+        int[] occurance = new int[101];
+        int maxOccurance = 0, n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            occurance[arr.get(i)]++;
+            if (occurance[arr.get(i)] > maxOccurance) {
+                maxOccurance = occurance[arr.get(i)];
+            }
+        }
+
+        return n - maxOccurance;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
